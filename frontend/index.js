2bc8029302596dd4ad03e02b6511da890f9861f8
Rajesh p <prajeshyadav333@gmail.com>
2026-08-13 16:34:44 +0530

    toggle fun

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,22 @@
-// alert("welcome to web bootcamp");
+var user=[
+    {
+        "name":"John Doe",
+        "gender":"male",
+        "image":"john.png"
+    },
+    {
+        "name":"Jane Doe",
+        "gender":"female",
+        "image":"jane.png"
+    }
+]
+var curid=0;
+function toggleuser(){
+    curid=(curid+1)%2;
+    var userName = document.getElementById("user-name");
+    var userGender = document.getElementById("user-gender");
+    var userImage = document.getElementById("user-image");
+    userName.innerHTML=user[curid].name;
+    userGender.innerHTML=user[curid].gender;
+    userImage.src=user[curid].image;
+}
